c35dec67807c44194fcdeade78d86cc820c501c8
Denis <den.listapad@mail.ru>
2024-04-30 10:37:13 +0300

    first init commit in booking app

---
+++ src/booking_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import (
+    # some_view,
+    # MyView,
+    # some_template_view,
+    home_view,
+    user_comment_view,
+)
+
+urlpatterns = [
+    # path('some_url/<int:some_int>/<str:some_str>', some_view),
+    # re_path(r"^some_new_url/(?P<year>[0-9]{1,3})$", MyView.as_view()),
+    # path('index', some_template_view),
+    path('home', home_view, name="home"),
+    path('user_comment', user_comment_view, name="user_comment")
+]
---
+++ src/booking_app/views.py
@@ -0,0 +1,78 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+
+# function base view
+def some_view(request, some_int, some_str):
+    return HttpResponse(f"<h1>hello django app some int: {some_int} some str: {some_str}</h1>")
+
+
+# def some_new_view(request, year):
+#     return HttpResponse(f"<h1>regular expression view  some year {year} </h1>")
+
+class MyView(View):
+    def get(self, request, year):
+        return HttpResponse(f"<h1>regular expression view  some year {year} </h1>")
+
+
+# class User:
+#     def __init__(self, name: str, age: int):
+#         self.name = name
+#         self.age = age
+#
+#     def get_info_user(self):
+#         return f"name :{self.name} age: {self.age}"
+
+
+# def some_template_view(request):
+#     # context = {
+#     #     "some_arg": "hello world",
+#     #     "some_list": [4, 2, 6, 1, 15, 16, 21],
+#     #     "user": User("Jhon", 45)
+#     # }
+#
+#     return render(
+#         request=request,
+#         template_name="base.html",
+#         # context=context
+#     )
+
+def home_view(request):
+    return render(
+        request=request,
+        template_name="home.html",
+    )
+
+
+comments = [
+    {
+        "number": 1,
+        "user_id": 1,
+        "name": "John",
+        "comment": "some John comment"
+    },
+    {
+        "number": 2,
+        "user_id": 2,
+        "name": "Ann",
+        "comment": "some Ann comment"
+    },
+    {
+        "number": 3,
+        "user_id": 3,
+        "name": "Peter",
+        "comment": "some Peter comment"
+    },
+]
+
+
+def user_comment_view(request):
+    context = {
+        "comments": comments
+    }
+    return render(
+        request=request,
+        template_name="user_comment.html",
+        context=context
+    )
