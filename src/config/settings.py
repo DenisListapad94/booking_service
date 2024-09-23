b39b77741e13049460f0ee4ca5a6a74746290630
Denis <den.listapad@mail.ru>
2024-09-23 21:53:09 +0300

    fix static

--- src/config/settings.py
+++ src/config/settings.py
@@ -154,10 +154,10 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media_files'
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/src/static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/src/static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
